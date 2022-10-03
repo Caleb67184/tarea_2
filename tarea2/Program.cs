ebb6d836d059b2932ce4f15d3e3d6ed2b4131a0c
Caleb <playcaleb1@gmail.com>
2022-10-02 20:39:53 -0600

    Add project files.

---
+++ tarea2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea2
+{
+    internal class Program
+    {
+        public static void aguinaldo(string nombre,float salario)
+        {
+            float aguinaldo = salario * 12;
+            Console.WriteLine("Nombre: "+nombre
+                +" ,Salario: "+salario
+                +" ,Aguinaldo: "+aguinaldo+"\n");
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+            string[] empleados = {"Mario","Luis","Carla","Nico","María"};
+            float[] salarios = { 2500, 6300, 9800, 1900, 7200 };
+
+            //Al final no entendí como hacerlo con un foreach
+            //asi que lo hice con el for de siempre
+            for (int i = 0; i < empleados.Length; i++)
+            {
+                aguinaldo(empleados[i], salarios[i]);
+            }
+            Console.Read();
+
+        }
+    }
+}
